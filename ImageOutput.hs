2f1af832bbeca7dd8de727d32c59e7d07cff7965
veniversum <bozotok@gmail.com>
2015-11-23 23:43:07 +0000

    Added some documentation Split gif output function

--- ImageOutput.hs
+++ ImageOutput.hs
@@ -9,11 +9,14 @@ createPngColor :: (Int,Int) -> (Int -> Int -> PixelRGB8) -> IO()
 createPngColor = createPngColorWithFileName "out_color.png"
 
 createPngColorWithFileName :: String -> (Int,Int) -> (Int -> Int -> PixelRGB8) -> IO()
-createPngColorWithFileName filename size pixelRenderer= writePng filename $ uncurry (generateImage pixelRenderer) size
+createPngColorWithFileName filename size pixelRenderer = writePng ("./out/" ++ filename) $ uncurry (generateImage pixelRenderer) size
 
 createImage :: Pixel a => (Int,Int) -> (Int -> Int -> a) -> Image a
 createImage size pixelRenderer = uncurry (generateImage pixelRenderer) size
 
 createGifColor :: [Image PixelRGB8]-> IO ()
-createGifColor frames = a
-    where Right a = writeGifAnimation "out_color.gif" 10 LoopingForever frames
+createGifColor = createGifColorWithFileName "out_color.gif"
+
+createGifColorWithFileName :: String -> [Image PixelRGB8]-> IO ()
+createGifColorWithFileName filename frames = a
+    where Right a = writeGifAnimation filename 10 LoopingForever frames
--- mbrot.hs
+++ mbrot.hs
@@ -5,25 +5,31 @@ import ImageOutput
 import Coloring
 import Codec.Picture.Types
 
--- Size parameters
+-- |Size parameters
 size :: (Int,Int)
 size = (1400,800)
 
---Helpful calculated parameters
+-- |Floating point size, calculated
 sizef :: (Float,Float)
 sizef = (fromIntegral *** fromIntegral) size
 
+-- |Maximum iterations
 maxIter :: Int 
 maxIter = 1000
 
+-- |Color step for integer esacpe coloring
 mult :: Float
 mult = 255 / fromIntegral maxIter
 
+-- |Color step for continuously colored
 multg :: Float
 multg = 400 / fromIntegral maxIter
 
--- Function for rendering pixels
-drawGreyscale :: Bool -> Int -> Int -> Pixel8
+-- |Function for rendering pixel in greyscale
+drawGreyscale :: Bool    -- ^Continuously colored
+              -> Int
+              -> Int
+              -> Pixel8
 drawGreyscale c x y 
     |c =  fromIntegral (round(float * mult))
     |otherwise = fromIntegral(int * round mult)
@@ -34,18 +40,21 @@ drawGreyscale c x y
           x2 = fst sizef /2
           y2 = snd sizef /2
 
---drawColor :: Int -> Int -> PixelRGB8
---drawColor x y =  PixelRGB8 (0::Pixel8) (0::Pixel8) (fromIntegral (int*mult))
---    where int = mbrot maxIter (((x1/x2)-0.5) :+ (y1/y2))
---          x1 = 2 * (fromIntegral x - x2)
---          y1 = 2 * (fromIntegral y - y2)
---          x2 = fst sizef /2
---          y2 = snd sizef /2
-----    |x < fst size = (writePixel image x y 255) >> draw image (x + 1) y
-----    |y < snd size = (writePixel image x y 255) >> draw image 0 (y+1)
-----    |otherwise = writePng "out.png" (freezeImage image)
----- (255 `mod` (mbrot maxIter (x :+ y)))
-drawColorZoom :: Float -> (Float,Float) -> Int -> Int -> PixelRGB8
+-- |Deprecated. Use 'drawColorZoom' instead
+drawColor :: Int -> Int -> PixelRGB8
+drawColor x y =  colorToPixel $ color (fromIntegral (int*400)/1000)
+    where int = mbrot maxIter ((x1/x2) :+ (y1/y2))
+          x1 = 1.75 * (fromIntegral x - x2) 
+          y1 = (fromIntegral y - y2)
+          x2 = fst sizef /2
+          y2 = snd sizef /2
+
+-- |Function for rendering pixel in color, with zoom level and focus
+drawColorZoom :: Float         -- ^ Zoom level
+              -> (Float,Float) -- ^ Focus point (x,y)
+              -> Int 
+              -> Int 
+              -> PixelRGB8
 drawColorZoom zoom (fx, fy) x y =  colorToPixel $ color (float*multg)
     where float = mbrotContinuous maxIter (((x1/x2)+fx) :+ ((y1/y2)+fy))
           x1 = 1.75 * (fromIntegral x - x2) / zoom
@@ -53,10 +62,15 @@ drawColorZoom zoom (fx, fy) x y =  colorToPixel $ color (float*multg)
           x2 = fst sizef /2
           y2 = snd sizef /2
 
+-- |Function for drawing color gradient. Tests for correct linear interpolation of color
 drawTestGradient :: Int -> Int -> PixelRGB8
 drawTestGradient x _ = colorToPixel $ color (fromIntegral x *400/1000)
 
-mbrot :: RealFloat x => Int -> Complex x -> Int
+-- |Function for integer escape time of mbrot set of Re(x) + Im(y)
+mbrot :: RealFloat x
+     => Int           -- ^ Maximum iterations
+     -> Complex x     -- ^ Re(x) + Im(y)
+     -> Int
 mbrot n = mbrot' maxIter (0 :+ 0)
     where
         mbrot' :: RealFloat x => Int -> Complex x -> Complex x -> Int
@@ -66,7 +80,11 @@ mbrot n = mbrot' maxIter (0 :+ 0)
                 where 
                     newIter = a*a + c
 
-mbrotContinuous :: RealFloat x => Int -> Complex x -> x
+-- |Function for real floating escape time of mbrot set of Re(x) + Im(y)
+mbrotContinuous :: RealFloat x
+                => Int           -- ^ Maximum iterations
+                -> Complex x     -- ^ Re(x) + Im(y)
+                -> x
 mbrotContinuous n = mbrot' maxIter (0 :+ 0)
     where
         mbrot' :: RealFloat x => Int -> Complex x -> Complex x -> x
@@ -81,10 +99,3 @@ mbrotContinuous n = mbrot' maxIter (0 :+ 0)
                                     --fromIntegral (maxIter - n +1) - (log (log (magnitude newIter)))/ log (2.0)
                                   else fromIntegral maxIter
 
-main :: IO()
-main = 
-            --createPngColor (1000,50) (drawTestGradient)
-            createPngColor size (drawColorZoom 1 ((-0.743643887037158704752191506114774),(0.131825904205311970493132056385139)))  -- Create continuous colored static image
-            --createPngColor size (drawColorZoom 2 (-0.75,0))
-            --createPngGreyscale size (drawGreyscale True)  -- Create continuous greyscale static image
-            --createGifColor [createImage size (drawColorZoom i) | i <- [1..100]] -- Create continuously colored zooming gif
