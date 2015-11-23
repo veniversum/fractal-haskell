6476e168697d502c66d7dc7b024397221c4fa5ab
veniversum <bozotok@gmail.com>
2015-11-23 02:57:19 +0000

    Added color png output Added animated gif output Added BSD3 license file

--- imageoutput.hs
+++ imageoutput.hs
@@ -1,6 +1,15 @@
-module ImageOutput (Pixel8, createPngGreyscale) where
+module ImageOutput where
 import Codec.Picture
 import Codec.Picture.Types
 
 createPngGreyscale :: (Int,Int) -> (Int -> Int -> Pixel8) -> IO()
-createPngGreyscale size pixelRenderer= writePng "out.png" $ uncurry (generateImage pixelRenderer) size
+createPngGreyscale size pixelRenderer= writePng "out_grey.png" $ uncurry (generateImage pixelRenderer) size
+
+createPngColor :: (Int,Int) -> (Int -> Int -> PixelRGB8) -> IO()
+createPngColor size pixelRenderer= writePng "out_color.png" $ uncurry (generateImage pixelRenderer) size
+
+createImage :: Pixel a => (Int,Int) -> (Int -> Int -> a) -> Image a
+createImage size pixelRenderer = uncurry (generateImage pixelRenderer) size
+
+createGifColor :: [Image PixelRGB8]-> Either String (IO ())
+createGifColor = writeGifAnimation "out_color.gif" 10 LoopingForever 
--- mbrot.hs
+++ mbrot.hs
@@ -1,6 +1,8 @@
 import Data.Complex
 import Control.Arrow ((***))
 import ImageOutput
+import Codec.Picture.Types
+
 
 -- Size parameters
 size :: (Int,Int)
@@ -13,8 +15,17 @@ maxIter :: Int
 maxIter = 20
 
 -- Function for rendering pixel
-draw :: Int -> Int -> Pixel8
-draw x y =  fromIntegral (int*mult)
+drawGreyscale :: Int -> Int -> Pixel8
+drawGreyscale x y =  fromIntegral (int*mult)
+    where int = mbrot maxIter (((x1/x2)-0.5) :+ (y1/y2))
+          x1 = 2 * (fromIntegral x - x2)
+          y1 = 2 * (fromIntegral y - y2)
+          x2 = fst sizef /2
+          y2 = snd sizef /2
+          mult = 255`div`maxIter
+
+drawColor :: Int -> Int -> PixelRGB8
+drawColor x y =  PixelRGB8 (0::Pixel8) (0::Pixel8) (fromIntegral (int*mult))
     where int = mbrot maxIter (((x1/x2)-0.5) :+ (y1/y2))
           x1 = 2 * (fromIntegral x - x2)
           y1 = 2 * (fromIntegral y - y2)
@@ -25,6 +36,14 @@ draw x y =  fromIntegral (int*mult)
 --    |y < snd size = (writePixel image x y 255) >> draw image 0 (y+1)
 --    |otherwise = writePng "out.png" (freezeImage image)
 -- (255 `mod` (mbrot maxIter (x :+ y)))
+drawColor2 :: Float -> Int -> Int -> PixelRGB8
+drawColor2 zoom x y =  PixelRGB8 (0::Pixel8) (0::Pixel8) (fromIntegral ((maxIter-int)*mult))
+    where int = mbrot maxIter (((x1/x2)-0.743643887037158704752191506114774) :+ ((y1/y2)+0.131825904205311970493132056385139))
+          x1 = 2 * (fromIntegral x - x2) / zoom
+          y1 = 2 * (fromIntegral y - y2) / zoom
+          x2 = fst sizef /2
+          y2 = snd sizef /2
+          mult = 255`div`maxIter
 
 mbrot :: RealFloat x => Int -> Complex x -> Int
 mbrot n = mbrot' maxIter (0 :+ 0)
@@ -37,4 +56,4 @@ mbrot n = mbrot' maxIter (0 :+ 0)
                     newIter = a*a + c
 
 main :: IO()
-main = createPngGreyscale size draw
+main = createPngColor size (drawColor2 2)
