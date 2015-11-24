bee6398ea937fae8f5737cbfc346a5ed1219daff
veniversum <bozotok@gmail.com>
2015-11-24 23:26:13 +0000

    Add more sample render functions

--- src/generateFrame.hs
+++ src/generateFrame.hs
@@ -7,4 +7,9 @@ import Codec.Picture.Types
 main :: IO()
 main = do
     args <- getArgs
-    createPngColorWithFileName ("frame"++(head args :: String)++".png") size (drawColorZoom (1.1**(read $ head args :: Float)) ((-0.743643887037158704752191506114774),(0.131825904205311970493132056385139)))
+    -- Zooming animation at fixed point
+    --createPngColorWithFileName ("frame"++(head args :: String)++".png") size (drawColorZoom (1.1**var) ((-0.743643887037158704752191506114774),(0.131825904205311970493132056385139)))    
+    
+    -- Zooming animation focused at moving point
+    createPngColorWithFileName ("frame"++(head args :: String)++".png") size (drawColorZoom (2 * (1.07032027027074**(read $ head args :: Float))) ((-1-((read $ head args :: Float)/21*0.31)),(0)))
+        
--- src/generateStatic.hs
+++ src/generateStatic.hs
@@ -12,8 +12,16 @@ main = do
 
 draw :: Int -> IO()
 draw 0 = createPngColorWithFileName "gradient.png" (fst size,50) (drawTestGradient) -- Outputs test gradient
-draw 1 = createPngColor size (drawColorZoom 1 ((-0.743643887037158704752191506114774),(0.131825904205311970493132056385139)))  -- Create continuous colored static image
-draw 2 = createPngColor size (drawColorZoom 1 (-0.75,0))
-draw 3 = createPngGreyscale size (drawGreyscale True)  -- Create continuous greyscale static image
-draw 4 = createGifColor [createImage size (drawColorZoom i (-0.75,0)) | i <- [1..100]] -- Create continuously colored zooming gif
-draw 5 = createPngColor size (drawColorZoom  1345 ((-0.7435669),(0.1314023)))
+-- Test reference images
+draw 1 = createPngColor size (drawColorZoom 1 (-0.75,0))-- Create continuous colored static image
+draw 2 = createPngGreyscale size (drawGreyscale True)  -- Create continuous greyscale static image
+-- Interesting zoom patterns
+draw 3 = createPngColorWithFileName "zoom1.png" size (drawColorZoom 1345 ((-0.7435669),(0.1314023))) --zoom 1
+draw 4 = createPngColorWithFileName "zoom2.png" size (drawColorZoom 4169 ((-0.74364990),(0.13188204))) --zoom 2
+draw 5 = createPngColorWithFileName "zoom3.png" size (drawColorZoom 25497 ((-0.74364085),(0.13182733))) --zoom 3
+
+-- Run with high bailout! at least maxIter = 5000
+draw 6 = createPngColorWithFileName "zoom4.png" size (drawColorZoom 210350 ((-0.743643135),(0.131825963))) --zoom 4
+draw 7 = createPngColorWithFileName "zoom5.png" size (drawColorZoom 1048800 ((-0.743644786),(0.1318252536))) --zoom 5
+
+draw 8 = createPngColorWithFileName "zoom6.png" size (drawColorZoom 1345 ((-0.7435669),(0.1314023))) --zoom 6
